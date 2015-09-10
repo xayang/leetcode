ca78a69815b871caeaf74f137c96b8f82eeab574
Alice Yang <alicexiaohongyang@yahoo.com>
2015-09-10 17:00:07 -0400

    Create solution.java

---
+++ Java/solution.java
@@ -0,0 +1,114 @@
+public class Solution {
+    public static  ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+         ArrayList<Integer> lst = new ArrayList<Integer>();
+ 
+        if(root == null)
+            return lst; 
+ 
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        //define a pointer to track nodes
+        TreeNode p = root;
+ 
+        while(!stack.empty() || p != null){
+ 
+            // if it is not null, push to stack
+            //and go down the tree to left
+            if(p != null){
+                stack.push(p);
+                p = p.left;
+ 
+            // if no left child
+            // pop stack, process the node
+            // then let p point to the right
+            }else{
+                TreeNode t = stack.pop();
+                lst.add(t.val);
+                p = t.right;
+            }
+        }
+ 
+        return lst;
+    }
+    
+    public static ArrayList<Integer> postorderTraversal(TreeNode root) {
+    	 
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+ 
+        if(root == null)
+            return lst; 
+ 
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+ 
+        TreeNode prev = null;
+        while(!stack.empty()){
+            TreeNode curr = stack.peek();
+ 
+            // go down the tree.
+            //check if current node is leaf, if so, process it and pop stack,
+            //otherwise, keep going down
+            if(prev == null || prev.left == curr || prev.right == curr){
+                //prev == null is the situation for the root node
+                if(curr.left != null){
+                    stack.push(curr.left);
+                }else if(curr.right != null){
+                    stack.push(curr.right);
+                }else{
+                    stack.pop();
+                    lst.add(curr.val);
+                }
+ 
+            //go up the tree from left node    
+            //need to check if there is a right child
+            //if yes, push it to stack
+            //otherwise, process parent and pop stack
+            }else if(curr.left == prev){
+                if(curr.right != null){
+                    stack.push(curr.right);
+                }else{
+                    stack.pop();
+                    lst.add(curr.val);
+                }
+ 
+            //go up the tree from right node 
+            //after coming back from right node, process parent node and pop stack. 
+            }else if(curr.right == prev){
+                stack.pop();
+                lst.add(curr.val);
+            }
+ 
+            prev = curr;
+        }
+ 
+        return lst;
+    }
+
+    
+    public static void main(String[] args){
+    	TreeNode node1 = new TreeNode(1);
+    	TreeNode node2 = new TreeNode(2);
+    	TreeNode node3 = new TreeNode(3);
+    	TreeNode node4 = new TreeNode(4);
+    	TreeNode node5 = new TreeNode(5);
+    	
+    	node2.setLeft(node4);
+    	node2.setRight(node5);
+    	
+    	node1.setLeft(node2);
+    	node1.setRight(node3);
+    	
+    	List<Integer> result = Solution.inorderTraversal(node1);
+    	
+    	System.out.println(result);
+    	
+    	 result = Solution.postorderTraversal(node1);
+     	
+     	System.out.println(result);
+    	
+    	
+    	
+    	
+    }
+}
