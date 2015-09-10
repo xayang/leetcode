f634afa1e0240e8047e8f4e74f925e87351d56db
Alice Yang <alicexiaohongyang@yahoo.com>
2015-09-10 16:59:30 -0400

    Create TreeNode.java

---
+++ Java/TreeNode.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+	public TreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+	public TreeNode getRight() {
+		return right;
+	}
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+    
+}
